674b64ad65a2afc525091e0641a5ca94c2327ccc
Kevin Davila <kfariidavila@gmail.com>
2023-06-09 23:06:40 -0500

    Fixed guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,32 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-  Router,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(private router: Router) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    if (localStorage.getItem('auth')) {
-      // User is logged in, allow navigation
-      return true;
-    } else {
-      // User is not logged in, redirect to login page
-      this.router.navigate(['/login']);
-      return false;
-    }
+  canActivate() {
+    if (!localStorage.getItem('auth')) this.router.navigate(['/login']);
+    return false;
   }
 }
